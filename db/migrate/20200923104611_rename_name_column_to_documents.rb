7dc7957a5cd26cebe088ad59f652b0fd6d806f79
bata1016 <blp0087bata@gmail.com>
2020-10-02 18:18:10 +0900

    Update 20200923104611_rename_name_column_to_documents.rb

--- db/migrate/20200923104611_rename_name_column_to_documents.rb
+++ db/migrate/20200923104611_rename_name_column_to_documents.rb
@@ -1,5 +1,5 @@
 class RenameNameColumnToDocuments < ActiveRecord::Migration[6.0]
   def change
-    rename_column :documents, :name, :document_tag
+    rename_column :documents, :name, :document_name
   end
 end
