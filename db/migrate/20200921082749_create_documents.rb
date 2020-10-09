696c344efa9980ec059246e37f0d56c5cf86e3f7
bata1016 <blp0087bata@gmail.com>
2020-10-09 16:41:25 +0900

    マイグレーション訂正

--- db/migrate/20200921082749_create_documents.rb
+++ db/migrate/20200921082749_create_documents.rb
@@ -1,7 +1,7 @@
 class CreateDocuments < ActiveRecord::Migration[6.0]
   def change
     create_table :documents do |t|
-      t.string :name, null: false
+      t.string     :name, null: false
       t.references :folder, null: false, foreign_key: true
       t.references :user  , null: false, foreign_key: true
       t.timestamps
