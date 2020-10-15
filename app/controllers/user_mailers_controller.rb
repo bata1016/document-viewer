2022e689489ecb094d8f5d89d82b3f40cfda4d16
bata1016 <blp0087bata@gmail.com>
2020-10-15 12:40:22 +0900

    メール作成フロント実装

--- app/controllers/user_mailers_controller.rb
+++ app/controllers/user_mailers_controller.rb
@@ -2,5 +2,20 @@ class UserMailersController < ApplicationController
   def new
     @folder = Folder.find(params[:folder_id])
     @document = Document.find(params[:document_id])
+    @user_name = current_user.name
+    @user_mail = current_user.email
+    document = Document.find(params[:document_id])
+    @images = document.images
   end
+
+  # def create 
+  #   UserMailer.
+  # end
+
+  private
+  def email_params
+    params.permit(:send_email, :subject, :message)
+  end
+
+
 end
