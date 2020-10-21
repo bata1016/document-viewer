742efadaf2635ba6dadef4f3477360f5b6577132
bata1016 <blp0087bata@gmail.com>
2020-10-21 17:01:04 +0900

    ゲストログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: "folders#index"
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   resources :folders, only: [:new, :create, :destroy] do
     resources :documents, only: [:index, :new, :create, :edit, :update, :destroy ] do
       resources :user_mailers, only: [:new, :create]
