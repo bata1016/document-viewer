54dbb4da78de527d913a18dd0d220c191fb43763
bata1016 <blp0087bata@gmail.com>
2020-12-24 20:25:56 +0900

    修正

--- db/migrate/20200923104731_rename_name_column_to_folders.rb
+++ db/migrate/20200923104731_rename_name_column_to_folders.rb
@@ -1,6 +1,6 @@
 class RenameNameColumnToFolders < ActiveRecord::Migration[6.0]
   def change
-    # rename_column :folders, :name, :folder_name
+    rename_column :folders, :name, :folder_name
     add_index     :folders, [:user_id, :folder_name], unique: true
   end
 end
